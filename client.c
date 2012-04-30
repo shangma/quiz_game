385e8b475dc2181780802431792c9140788a40ba
Jake Johnson <kognizant.kog@gmail.com>
2012-04-30 16:43:33 -0400

    Added preprocessor directives for OS identification

--- client.c
+++ client.c
@@ -2,8 +2,8 @@
                                 client.c
  * usage: client.c <host>
  
- * Connects to Host to Get Questions, and Answers
- * Then sends answers to Host and finds out if winner
+ * duties: * allows for joining or registering		[ ]
+		   * responds to servers questions			[ ]
  
  TODO: Documentation
  TODO: timeout?
@@ -16,66 +16,151 @@
 #include <stdio.h>
 #include <cnaiapi.h>
 
-#define BUFFSIZE		256
-#define INPUT_PROMPT		"Answer > "
+#define BUFFSIZE			256
+// define value of user input prompt
+#define INPUT_PROMPT		"Answer      > "
+// define comical reveived prompt
 #define RECEIVED_PROMPT		"Alex Trebek > "
 
-int readln(char *, int);                // WHERE IS THIS DEFINED?
+//////////////////////////////////////////////////////////////////////
+//------------A NOTE ON FUNCTIONS PREFIXED WITH (void)--------------//
+//                                                                  //
+//   Placing '(void)' before a function call works like any other   //
+//   typecast. It is used to cast functions that normally return a  //
+//   value as void. Put plainly, it says, this function returns     //
+//   some value, but I am not storing or using that value in any    //
+//   way, so don't worry about returning a value at all.            //
+//                                                                  //
+//////////////////////////////////////////////////////////////////////
+
+// readln reads from stdin stream until
+// EOF or buffer is full
+// location: ../anaiapi/apps/readln.c
+int readln(char *, int); 
+
+
+// ------- FUNCTIONS WHICH ARE DEFINED BELOW ------- //
+
+// timeout(): times-out cliets who fail to 
+//   repond within predetermined time limit
 int timeout();
+
+// verify_input(): 
+//   TAKES: a charater
+//
+//   RETURNS: boolean value indicating validity 
+//            either 0 (valid) or 1 (invalid)
 int verify_input(char);
+
+// convert_case(): 
+//   TAKES: a single character
+//
+//   RETURNS: if that character is either 'a', 'b', 'c', 
+//            or'd', funciton will return the uppercase 
+//            analog of supplied character; else the input
+//            value is returned unchaged
 char convert_case(char);
+
+// NOTE: computer is typedef of long
+//
+// make_connection(): 
+//   TAKES: an integer and a pointer to arguments supplied to 
+//          main() during command line call
+//
+//   RETURNS: value of datatype coputer (typedef of long)
 computer make_connection(int, char*[]);
 
-
 int main(int argc, char *argv[]){
+	// create variable of type long to identify a host computer
     computer comp;
+
+	// create variable of type short to represent an application
     appnum app = 20001;
+
+	// create a variable of type int to represent the client/host
+	// connection
     connection conn;
+
+	// create a buffer array of size 256 to store characters
     char buff[BUFFSIZE];
-    int	expect, received, len;
+
+	// declare three integers dealing with packages
+    int	expect, 
+		received, 
+		len;		// used to store buffer size
     
-    int answer_index = 0;
+    int answer_index = 0;             // does this return index of the beginning of an answer?
     char answers[21];
     int bad_input;
     
     // ***************** Check Args ************************//
+	// this program must have at least 1 & no more than 2
+	// arguments
     if (argc < 1 || argc > 2) {
+		// print a helpful usage  message
         (void) fprintf(stderr, "usage: %s <compname>\n", argv[0]);
         exit(1);
     }
     
     // *************** Make Connection ********************//
     (void) printf("Connecting");
+
+	// determine whether arguments supplied when program
+	// was called are valid; store returned IP address of
+	// host system
     comp = make_connection(argc, argv);
 
     // **************** Start Game ***************************** //
-    while((len = recvln(conn, buff, BUFFSIZE)) > 0){   //      Game is playing
-    //  Read Question
+
+	//////////////////////////////////////////////////////////////
+	// recvln(): 
+	//   TAKES: an conn (integer), character buffer of 256, & the
+	//          BUFFSIZE (256)
+	//
+	//   SIDE EFFECTS: makes call to winsocket api's recv() 
+	//                 which returns number of bytes received
+	//                 & and stores data recieved in the buffer
+	//                 that has been pointed to; recvln, does
+	//                 the same for the buffer it is given
+	//
+	//   RETURNS: number of bytes recieved
+	//////////////////////////////////////////////////////////////
+    while((len = recvln(conn, buff, BUFFSIZE)) > 0) {   //      Game is playing
+		//  Read Question
         (void) printf(RECEIVED_PROMPT);
+		// flush stdio stream
         (void) fflush(stdout);
+		// NOTE: STDOUT_FILENO is just a typedef set to one
+		// write() writes prints len number of bytes from
+		// buff into the command line
         (void) write(STDOUT_FILENO, buff, len);
         
-    //  Get Answer
-        do{
+		//  Get Answer
+        do {
             (void) printf(INPUT_PROMPT);
             (void) fflush(stdout);
+			// read BUFFSIZE bytes from buff
+			// stores number of bytes in len
             len = readln(buff, BUFFSIZE);
             bad_input = 0;
 
-            //  Verify
+            //  make character uppercase if 'a', 'b', 'c', or 'd'
             buff[0] = convert_case(buff[0]);
 
+			// validate that buff[0] contains either 'A', 'B', 
+			// 'C', or 'D'
             if(verify_input(buff[0]) != 0){
                 (void) printf("Bad Input\n");
                 bad_input = 1;
             }
-        }
-        while(bad_input != 0);
-    
-    //  Store Answer
+        } while(bad_input != 0);
+		
+
+		//  Store Answer
         answers[answer_index] = buff[0];
     
-    //  TODO: Timeout
+		//  TODO: Timeout: must time-out cliets 
+		//	who fail to repond within predetermined time limit
     
     }
     // ***************** Finish Game  ***************************//
@@ -87,58 +172,103 @@ int main(int argc, char *argv[]){
     return 0;
 }
 
-
-
- computer make_connection(int argc, char *argv[]){
+//////////////////////////////////////////////////////////////
+// make_connection(): 
+//   TAKES: an integer and a pointer to arguments supplied to 
+//          main() during command line call
+//
+//   RETURNS: value of datatype coputer (typedef of long)
+//////////////////////////////////////////////////////////////
+computer 
+make_connection(int argc, char *argv[]) {
+	// create variable of type long to hold a value that
+	// represents the host computer to be connected to
     computer comp;
-    appnum app = 20001;
-    connection conn;
-    char buff[BUFFSIZE];
-    int	expect, received, len;
-    
-    comp = cname_to_comp(argv[1]);
-    
-    if (comp == -1)
-    {
-            (void) fprintf(stderr,"comp == -1");
-            exit(1);
-    }
 
+	// create variable of type appnum (typedef of short)
+	// to represet app to run
+    appnum app = 20001;
+
+	// create variable of type connection (typedef of int)
+	// to represent connection
+    connection conn;
+
+    // create a buffer array of size 256 to store characters
+    char buff[BUFFSIZE];
+
+	// declare three integers dealing with packages
+    int	expect, 
+		received, 
+		len;		// used to store buffer size
+   
+	// look up host by name and return its IP address
+	// store returned value as type computer
+    comp = cname_to_comp(argv[1]);
+   
+	// if cname_to_comp() returns -1, exit with error
+    if (comp == -1) {
+ 		(void) fprintf(stderr,"comp == -1");
+		exit(1);
+	}
+
+	// open a new TCP connection to the specified IP address
+	// store returned connection type value
     conn = make_contact(comp, app);
-    if (conn < 0)
-    {
+
+	// if make_contact returns a negative value, exit with
+	// error
+    if (conn < 0) {
         (void) fprintf(stderr,"conn < 0");
         exit(1);
     }
     
+	// if all has gone as planned, tell user they are 
+	// connected
     (void) printf("Connected");    
     
+	// return host's IP address
     return comp;
 }
 
- int verify_input(char input){    
-     if (input == 'A' || input == 'B' || input == 'C' || input == 'D'){
-         return 0;
-     }
-     else{
-         return 1;
-     }
- }
+////////////////////////////////////////////////////////////////
+// verify_input(): 
+//   TAKES: a charater
+//
+//   RETURNS: boolean value indicating validity 
+//            either 0 (valid) or 1 (invalid)
+////////////////////////////////////////////////////////////////
+int 
+verify_input(char input) {    
+    if (input == 'A' || input == 'B' || input == 'C' || input == 'D'){
+        return 0;
+    }
+    else { return 1; }
+}
  
- char convert_case(char input){
-     switch (input){
-         case 'a':
-             input = 'A';
-             break;
-         case 'b':
-             input = 'B';
-             break;
-         case 'c':
-             input = 'C';
-             break;
-         case 'd':
-             input = 'D';
-             break;
-     }
-     return input;
+/////////////////////////////////////////////////////////////////
+// convert_case(): 
+//   TAKES: a single character
+//
+//   RETURNS: if that character is either 'a', 'b', 'c', 
+//            or'd', funciton will return the uppercase 
+//            analog of supplied character; else the input
+//            value is returned unchaged
+////////////////////////////////////////////////////////////////
+char 
+convert_case(char input) {
+    switch (input ){
+        case 'a':
+            input = 'A';
+            break;
+        case 'b':
+            input = 'B';
+            break;
+        case 'c':
+            input = 'C';
+            break;
+        case 'd':
+            input = 'D';
+            break;
+    }
+    return input;
 }
--- server.c
+++ server.c
@@ -1,7 +1,13 @@
 /*************************************************************************
                                 server.c
- usage: server.c
+ * usage: server.c
  
+ * duties: * sends questions to all clients				[ ]
+	       * records answers recieved by clients		[ ]
+		   * times-out clients who fail to respond		[ ]
+			 within set time limit						[ ]
+		   * announce winner of 100 question game		[ ]
+
  * TODO: Send Questions
  * TODO: Get ANswers
  * TODO: Calc Winner
@@ -11,25 +17,63 @@
  * TODO: Parse QUestions
  *************************************************************************/
 
-
-#include <pthread.h>
+// include standard libraries
 #include <stdlib.h>
 #include <stdio.h>
 #include <cnaiapi.h>
+#include <ctime.h>
+#include <time.h>
 
-#define BUFFSIZE		256
-#define CONNECTIONS             21
+// include POSIX system headers
+#ifdef __GNU__
+	#include <pthread.h>
+	#include <fcntl.h>
+	#include <unistd.h>
+	#include <sys/stat.h>
+#endif
+
+// include windows-specific headers
+#ifdef _WIN32
+	#include <process.h>
+#endif
+
+#define BUFFSIZE			256
+#define CONNECTIONS         21
+
+const int TIME_LIMIT	  = 45; // in seconds
+const int NO_OF_QUESTIONS = 10;
 
 char get_answer(int);
 char* get_all_answers();
 int ask_question(char[]);
-void *client_thread(void *);
+void* client_thread(void *);
 
-int main(int argc, char *argv[])
-{
+// declare POXIS functions
+#ifdef __GNU__
+	// Declare a global variable with array [2] of data type int.
+	int pipefd[2];
+	unsigned int thread_id;
+#endif
+
+// declare Windows-specific functions
+#ifdef _WIN32
+	void echothread(void *);
+#endif
+
+// count_down:
+//   TAKES: int representing the number of seconds to
+//          count down from
+// 
+//   RETURNS: int returning 0 if succesfully executed
+int count_down(int);
+
+int main(int argc, char *argv[]) {
+	// NOTE: connection is typedef of int
+	// declare array of 21 ints
     connection	conn[CONNECTIONS];
     int         connection_index = 0;
-    int		len;
+    int			len;
+	// declare character buffer of size 256
     char        buff[BUFFSIZE];
     int         loop;
     int         points[CONNECTIONS];
@@ -41,23 +85,120 @@ int main(int argc, char *argv[])
     }
     
     //  Create Clients
-    while(connection_index < 20){
-        conn=malloc(sizeof(connection));
-        *conn=await_contact((appnum)atoi(argv[1]));
+    while(connection_index < 20) {
+		// connection is a typedef of int, so its size is 
+		// 4 bytes (on most current intel CPUs); thus, malloc 
+		// will allocate a memory block of 4 bytes & return 
+		// a pointer to the adress of that block; this works 
+		// because conn is an array and so after declaration 
+		// it is simply a pointer to the memory address of 
+		// the first element of its array; this is called
+		// decaying; the array decays to a pointer; after 
+		// this line, the variable 'conn' will hold a pointer
+		// to a block of size 4 bytes
+        conn = malloc(sizeof(connection));
 
-        if (conn < 0){
+		
+		/////////////////////////////////////////////////////////////
+		// await_contact():
+		//   TAKES: accepts a connection on port a
+		// 
+		//   RETURNS: if no master sock is already open for the 
+		//            port, create one and record it in the port-to-
+        //            socket table
+		/////////////////////////////////////////////////////////////
+
+		// here conn (which is a pointer variable) is being 
+		// dereferenced, so here we are actually story
+		// a value beginning at the memory address
+		// referenced by the variable conn; also note that
+		// the return value of function atoi() is being 
+		// cast as tyoe appnum (short)
+        *conn = await_contact((appnum)atoi(argv[1]));
+
+		// if no connection, print error
+        if (conn < 0) {
             (void) fprintf(stderr, "conn < 0");
         }
-        else{
-            pthread_t newthread;
-            conn[connection_index] = pthread_create(&newthread, NULL, client_thread, conn);
+        else {
+			// Windows-specific
+			#ifdef _WIN32
+			#endif
+
+			// POSIX-specific
+			#ifdef __GNU__
+				// declare new thread identifier
+				pthread_t newthread;
+				// pthread_create() routine creates a new thread within a process;
+				// the new thread starts in client_thread; conn is the start
+				// argument to client_thread; the resulting thread is stored in
+				// index connection_index of conn array
+				conn[connection_index] = pthread_create(&newthread, NULL, client_thread, conn);
+			#endif
         }
 
         connection_index++;
     }
     
-    // OPEN FILES FOR READING!
+    // --------------------- FILE IO -----------------------------//
+
+	// questions.dat should be written in the following format
+	//////////////////////////////////////////////////////////
+	//question #
+	//question
+	//option a
+	//option b
+	//option c
+	//option d
+	//correct answer
+	//blank line
+	//repeat until no more questions
+	//////////////////////////////////////////////////////////
+	//  Example:
+	//////////////////////////////////////////////////////////
+	//1
+	//How much wood could a wookchuck chuck?
+	//a) 20 pounds
+	//b) 39 pounds
+	//c) 3 pounds
+	//d) none of the above
+	//d
+	//
+	//2
+	//How much money is in my pocket?
+	//a) 3 bucks
+	//b) 100 bucks
+	//c) 0 bucks
+	//d) 20 bucks
+	//c
+	//
+	//3
+	//////////////////////////////////////////////////////////
+	// . . . and so on
+	/////////////////////////////////////////////////////////
+
+	// declare an input file pointer
+	FILE * input_file_ptr;
+
+	// set fopen() mode to read
+	char * mode = "r";
+
+	// store address of file stream in pointer variable
+	input_file_ptr = fopen("questions.dat", mode);
+
+	// error check
+	if (input_file_ptr == NULL) {
+		fprintf(stderr, "Can't open input file questions.dat!\n");
+		exit(1);
+	}
     
+	int question_no = 1;
+	// read through file by line
+	while (len = readln(buff, BUFFSIZE) != EOF) {
+		// send question and potential answers to everyone
+
+	}
+
     for(loop = 0; loop < CONNECTIONS; loop++){
         //  Send Questions to Clients
         //  Get Answers   
@@ -71,25 +212,84 @@ int main(int argc, char *argv[])
     return 0;
 }
 
-
-struct client{
+// structure holding information about client
+struct client {
     int link;
     int client_num;
     char answers[21];
 };
 
-void *client_thread(void *value)
-{
-	int		len;
-	char	buff[BUFFSIZE];
+// POXIS-specific functions
+#ifdef __GNU__
+	void* 
+	client_thread(void *value) {
+		int		len;
+		char	buff[BUFFSIZE];
 	
-	connection conn= *(connection *)value;
+		connection conn= *(connection *)value;
 
-	while((len = recv(conn, buff, BUFFSIZE, 0)) > 0)
+		while((len = recv(conn, buff, BUFFSIZE, 0)) > 0)
+		{
+			(void) send(conn, buff, len, 0);
+		}		/* iterate, echoing all data received until end of file */
+		send_eof(conn);
+		free(value);
+		pthread_exit(NULL);
+	}
+
+	// thread entry point
+	void* 
+	pipe_writer_thread(void* arg) {
+
+	}
+
+	void 
+	create_thread() {
+	   pthread_create(&thread_id, (pthread_attr_t *)NULL, pipe_writer_thread, NULL);
+	}
+#endif
+
+// Windows-specific functions
+#ifdef _WIN32
+	void 
+	echothread(void *value)
 	{
-		(void) send(conn, buff, len, 0);
-	}		/* iterate, echoing all data received until end of file */
-	send_eof(conn);
-	free(value);
-	pthread_exit(NULL);
+		connection conn= *(connection *)value;
+
+		while((len = recv(conn, buff, BUFFSIZE, 0)) > 0)
+		{
+			(void) send(conn, buff, len, 0);
+		}
+		send_eof(conn);
+
+		free(&conn);
+		_endthread();
+	}
+#endif
+
+/////////////////////////////////////////////////////////////////
+// count_down:
+//   TAKES: int representing the number of seconds to
+//          count down from
+// 
+//   RETURNS: int returning 0 if succesfully executed
+/////////////////////////////////////////////////////////////////
+int 
+count_down(int num_of_seconds) {
+	// store start value for count down; this value is the
+	// number of seconds that have lapsed since the Epoch
+	time_t start_time = time (NULL);
+
+	// calculate end time
+	time_t end_time = start_time + num_of_seconds;
+
+	int time_elapsed;
+
+	// lcountdown
+	while(difftime(end_time, start_time) > 0) {
+		// store number of seconds left in countdown
+		time_elapsed = difftime(end_time, start_time);  // if timer is a thread this could be a pointer to some external variable
+	}
+
+	return 0;
 }
